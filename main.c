70e8e8914165dce99b404080ff0acc3ae38edb5b
unknown <ctobias409@gmail.com>
2025-11-14 13:37:24 +0800

    sample comment

---
+++ main.c
@@ -0,0 +1,20 @@
+//Tobias, Christine P. DIT 1-2
+
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    
+    // 1. Declaring variables
+    
+    int myAge = 18;
+    float myHeight = 1.64;
+    char myInitial = 'C';
+    bool isDITstudent = true;
+    
+    printf("Age:%d\n", myAge);
+    printf("Height:%.2f\n", myHeight);
+    printf("Initial:%c\n", myInitial);
+    printf("Student Status: %d", isDITstudent);
+   
+    return 0;
+}
